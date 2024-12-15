001ceec482b10d851da7bbfa5974f8151989df22
Your Name <you@example.com>
2024-12-15 17:32:46 +0200

    Answer Q2

--- Assingment3/Program.cs
+++ Assingment3/Program.cs
@@ -11,7 +11,13 @@
             //if ((Num % 4 == 0) && (Num % 3 == 0)) { Console.WriteLine("Yes"); }
             //else { Console.WriteLine("No"); }
             #endregion
+            #region Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive
+            //Console.WriteLine("Enter Number if it is negative number or positive :");
+            //int Num = int.Parse(Console.ReadLine());
 
+            //if ((Num > 0 )){ Console.WriteLine("positive"); }
+            //else { Console.WriteLine("Negative"); }
+            #endregion
 
             Console.ReadKey();
         }
