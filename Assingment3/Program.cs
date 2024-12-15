e864bf6cec8bc1f7da05d278a74ab5795cb296eb
Your Name <you@example.com>
2024-12-15 17:23:12 +0200

    Answer Q1

--- Assingment3/Program.cs
+++ Assingment3/Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             #region Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no
-            Console.WriteLine("Enter Number divided by 3 and 4 :");
-            int Num = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Number divided by 3 and 4 :");
+            //int Num = int.Parse(Console.ReadLine());
 
-            if ((Num % 4 == 0) && (Num % 3 == 0)) { Console.WriteLine("Yes"); }
-            else { Console.WriteLine("No"); }
+            //if ((Num % 4 == 0) && (Num % 3 == 0)) { Console.WriteLine("Yes"); }
+            //else { Console.WriteLine("No"); }
             #endregion
 
 
