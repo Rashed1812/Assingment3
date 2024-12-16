a33d147c3fc8fb8668e17ec2b8af7141c5b779bd
Your Name <you@example.com>
2024-12-16 18:27:28 +0200

    Answer Q9

--- Assingment3/Program.cs
+++ Assingment3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assingment3
+﻿using System.Diagnostics.Metrics;
+
+namespace Assingment3
 {
     internal class Program
     {
@@ -73,6 +75,29 @@
             //float Power = float.Parse(Console.ReadLine());
 
             //Console.WriteLine(Math.Pow(inputNum, Power));
+            #endregion
+
+            #region Write a program to enter marks of five subjects and calculate total, average and percentage
+            //Console.WriteLine("Enter Marks of five subjects");
+
+            //Console.Write("Subject 1: ");
+            //int subject1 = int.Parse(Console.ReadLine());
+            //Console.Write("Subject 2: ");
+            //int subject2 = int.Parse(Console.ReadLine());
+            //Console.Write("Subject 3: ");
+            //int subject3 = int.Parse(Console.ReadLine());
+            //Console.Write("Subject 4: ");
+            //int subject4 = int.Parse(Console.ReadLine());
+            //Console.Write("Subject 5: ");
+            //int subject5 = int.Parse(Console.ReadLine());
+
+            //int total = subject1 + subject2 + subject3 + subject4 + subject5;
+            //double average = total / 5;
+            //double percentage = (total / 500.0) * 100;
+
+            //Console.WriteLine($"Total = {total}");
+            //Console.WriteLine($"Average = {average}");
+            //Console.WriteLine($"Percentage = {percentage}%");
             #endregion
             Console.ReadKey();
         }
